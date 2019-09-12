8f5cb975a1ebfcac1c6b11f49a7b574208b34dea
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-12 04:19:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ def line(array)
   if array.length < 1 
     puts "The line is currently empty."
   elsif array.length >= 1
-    pos = 1 
+    pos = 1
+    count = 0
     currLine = "The line is currently: "
     while count < array.length - 1
       currLine += "#{pos}. " + array[count] + " "
