d1e8983808d847d209a94ae28b63714b365346b8
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-12 04:04:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,8 @@ def line(array)
   elsif array.length >= 1
     count = 1 
     currLine = "The line is currently: "
-    array.join(" ")
     while count <= array.length
-      currLine += "#{count}. " + array[count-1]
+      currLine += "#{count}. " + array[count-1] + " "
       count += 1
     end
     puts currLine
