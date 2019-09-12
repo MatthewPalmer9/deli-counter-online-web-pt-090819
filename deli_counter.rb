8fa616e20cbbae3ed8606c8977f819d721cd5e89
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-12 03:11:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,9 @@
 def line(array)
   if array.length < 1 
     puts "The line is currently empty."
-  end
+  elsif array.length >= 1
+    count = 0
+    puts "The line is currently: " + array.each(){|person| 
+      array[count+1].index
+    }
 end
