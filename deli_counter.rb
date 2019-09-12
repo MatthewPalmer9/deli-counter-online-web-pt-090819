d5e591a992a466acf229484c2af4f77a250d5110
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-12 03:30:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     count = 1
     currentLine = "The line is currently: "
     while count <= array.length
-      currentLine += "#{count}. " + array[count-1] + " "
+      currentLine += "#{count}. " + array[count-1]
       count += 1 
     end
   end
