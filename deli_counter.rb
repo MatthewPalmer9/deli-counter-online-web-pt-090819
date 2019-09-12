f7a151777e1090034b2fa405bd60232b3533423c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-12 04:13:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,10 +7,6 @@ def line(array)
     array[0..-2].join(" ")
     while count <= array.length
       currLine += "#{count}. " + array[count-1]
-      count += 1
-    end
-    newLine = currLine.split()
-    finalLine = newLine[0..-2].join(" ")
-    puts finalLine
+      
   end
 end
